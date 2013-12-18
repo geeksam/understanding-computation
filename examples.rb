64041a25b52312970db69dd53bba6b7e4f7b12ae
Sam Livingston-Gray <geeksam@gmail.com>
2013-12-17 21:57:10 -0800

    Rename examples.rb to run_examples, make it executable, and have it run all chapter examples

--- examples.rb
+++
@@ -1,9 +0,0 @@
-# try running this with:  irb -r ./examples.rb
-
-require_relative 'simple'
-
-@expr = Add.new(
-  Multiply.new(Number.new(1), Number.new(2)),
-  Multiply.new(Number.new(3), Number.new(4))
-)
-
