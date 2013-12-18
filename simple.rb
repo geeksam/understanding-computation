b364ade0e22ac38ac3daaa7d60ae2772adcc2b20
Sam Livingston-Gray <geeksam@gmail.com>
2013-12-17 20:08:41 -0800

    Page 27 adds #reduce

--- simple.rb
+++ simple.rb
@@ -61,3 +61,29 @@ class Multiply
     true
   end
 end
+
+
+### Page 27
+class Add
+  def reduce
+    if left.reducible?
+      Add.new(left.reduce, right)
+    elsif right.reducible?
+      Add.new(left, right.reduce)
+    else
+      Number.new(left.value + right.value)
+    end
+  end
+end
+
+class Multiply
+  def reduce
+    if left.reducible?
+      Multiply.new(left.reduce, right)
+    elsif right.reducible?
+      Multiply.new(left, right.reduce)
+    else
+      Number.new(left.value * right.value)
+    end
+  end
+end
