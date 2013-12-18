e3fe4abef4111f1051ed6a1e591fc7ffffc9b41f
Sam Livingston-Gray <geeksam@gmail.com>
2013-12-17 17:33:32 -0800

    Page 26 adds #reducible?

--- simple.rb
+++ simple.rb
@@ -41,3 +41,23 @@ class Multiply
     "«#{self}»"
   end
 end
+
+
+### Page 26
+class Number
+  def reducible?
+    false
+  end
+end
+
+class Add
+  def reducible?
+    true
+  end
+end
+
+class Multiply
+  def reducible?
+    true
+  end
+end
