325780ca575f8d57771808c77128aa8d4df5743f
Sam Livingston-Gray <geeksam@gmail.com>
2013-12-17 17:29:22 -0800

    Page 24

--- simple.rb
+++ simple.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 ### Page 23
 class Number < Struct.new(:value)
 end
@@ -6,4 +8,36 @@ class Add < Struct.new(:left, :right)
 end
 
 class Multiply < Struct.new(:left, :right)
+end
+
+
+### Page 24
+class Number
+  def to_s
+    value.to_s
+  end
+
+  def inspect
+    "«#{self}»"
+  end
+end
+
+class Add
+  def to_s
+    "#{left} + #{right}"
+  end
+
+  def inspect
+    "«#{self}»"
+  end
+end
+
+class Multiply
+  def to_s
+    "#{left} * #{right}"
+  end
+
+  def inspect
+    "«#{self}»"
+  end
 end
