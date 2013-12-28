2f58cfc91afccc2f2a0f2b7afb7b1d90cfa51e45
Sam Livingston-Gray <geeksam@gmail.com>
2013-12-27 21:39:19 -0800

    UTF-8 encode the string used to capture output

--- helpers/io_capture.rb
+++ helpers/io_capture.rb
@@ -17,6 +17,7 @@ end
 def capture_output_on(object)
   object.extend(IO_Attr) unless object.kind_of?(IO_Attr)  # punch that duck
   s = ""
+  s.encode! "UTF-8" # so it can handle those weird quote marks Tom loves so much
   object.io = StringIO.new(s)
   yield s
 end
