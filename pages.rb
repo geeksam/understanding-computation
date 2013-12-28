724dd1047eeffef2e3eeae204bae991bfb31d35d
Sam Livingston-Gray <geeksam@gmail.com>
2013-12-27 21:16:38 -0800

    I'm getting tired of creating page files manually

--- pages.rb
+++ pages.rb
@@ -1,8 +1,17 @@
 require 'pathname'
+require 'fileutils'
+
 @page_dir = Pathname.new(File.expand_path(File.dirname(__FILE__))).join('pages')
 
 def page(page_number)
   page_file = 'page-%03d' % page_number
+  ensure_page_file_exists page_file
   require @page_dir.join(page_file)
   yield if block_given?
+end
+
+def ensure_page_file_exists(page_file)
+  page_file_rb = @page_dir.join(page_file + '.rb')
+  return if File.exists?(page_file_rb)
+  FileUtils.cp @page_dir.join('template'), page_file_rb
 end
